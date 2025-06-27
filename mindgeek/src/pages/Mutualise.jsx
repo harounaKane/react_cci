7bc5511aef5460713626b52ab915b5d7665fa4d1
harounaKane <harouna.Kane@hotmail.fr>
2025-06-27 16:38:42 +0200

    API

--- mindgeek/src/pages/Mutualise.jsx
+++ mindgeek/src/pages/Mutualise.jsx
@@ -1,6 +1,17 @@
+// import { useEffect, useState } from "react";
 import MutualiseComponent from "../components/MutualiseComponent";
 
 export default function Mutualise(){
+    // const [users, setUser] = useState([]);
+
+    // useEffect( () => {
+    //     fetch("https://jsonplaceholder.typicode.com/users")
+    //     .then(ressult => ressult.json())
+    //     .then(data => setUser(data))
+    //     .catch( err => console.log("Erreur apel API: ", err)
+    //     )
+    // }); 
+
     return(
         <>
             <h3>Le serveur mutualisé</h3>
@@ -9,6 +20,21 @@ export default function Mutualise(){
             </p>
 
             <section className="pages">
+                {/* {
+                    users.map( user => (
+                        <MutualiseComponent 
+                            titre={user.name} 
+                            desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, deserunt!"
+                            detail={<ul>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                                </ul>}
+                            prix="2,99"
+                            />
+                    ) )
+                } */}
+
                 <MutualiseComponent 
                 titre="Perso - 100 Go" 
                 desc="Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, deserunt!"
@@ -39,6 +65,7 @@ export default function Mutualise(){
                     </ul>}
                 prix="2,99"
                 />
+                
             </section>
         </>
     )
